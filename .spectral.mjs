7235041ac7c9952f55176a07c718bd768acdfb49
api@fasttrack.net <api@fasttrack.net>
2023-01-10 14:56:07 -0500

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MjQ/spectral.js?branch_slug=master&token=4fa89178-d37e-4993-b415-335e071908b1";
+export default { extends: ruleset };
